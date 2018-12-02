59123b8ecf281b83fdc8818e3eb186acabebc2bd
sundarkas <sundarkas@gmail.com>
2018-12-02 13:01:45 +0530

    FirstSamples

---
+++ ReadnWrite.py
@@ -0,0 +1,4 @@
+name=input('What your name ? ')
+print('MyName is ',name,' Welcome to python!!')
+print('MyName is '+name+' Welcome to python!!')
+print('MyName is ',name+', Welcome to python!!')
---
+++ circleDemo.py
@@ -0,0 +1,6 @@
+from math import pi
+r=input('Circle Raidus ? ')
+print('Area of circel :',float(r)**2*pi)
+print('circumference ',2*float(r)*pi)
+print('Area of circel :',int(r * pi))
+print('circumference ',int(2*(r)*pi))
